0251877934d68727a5b535f095d293714671f5e5
TanabeHitoshi <tanabe2852@yahoo.co.jp>
2014-11-09 01:37:36 +0900

    now

---
+++ drive.h
@@ -0,0 +1,15 @@
+/*
+ * drive.h
+ *
+ *  Created on: 2014/11/09
+ *      Author: ｈｔ
+ */
+
+#ifndef DRIVE_H_
+#define DRIVE_H_
+
+#define GYRO_OFFSET 600 /* ジャイロセンサオフセット値(角速度0[deg/sec]時) 600*/
+
+static void line_follow(int speed, int turn, int gyro);
+
+#endif /* DRIVE_H_ */
