d6d5a04ce4597e9c0f68a4e025abbcc627bcc58c
TanabeHitoshi <tanabe2852@yahoo.co.jp>
2015-03-11 22:15:10 +0900

    add drive

--- sample_pid.c
+++ sample_pid.c
@@ -14,7 +14,7 @@
 #include "balancer.h" /* 倒立振子制御用ヘッダファイル */
 #include "ini.h"
 #include "Seesaw.h"
-//#include "drive.h"
+#include "drive.h"
 
 /* OSEK declarations */
 DeclareCounter(SysTimerCnt);
@@ -933,30 +933,7 @@ void calibration(int *black,int *white,int angle)
 	ecrobot_sound_tone(440, 170, 100);
 }
 #if 1
-//*****************************************************************************
-// 関数名 : line_follow
-// 引数 : speed, turn 走行速度、旋回速度
-// 引数 : gyro_sensor　ジャイロセンサー値
-// 返り値 : 無し
-// 概要 : ライントレース
-//*****************************************************************************
-void line_follow(int speed, int turn, int gyro_sensor)
-{
-	signed char pwm_L, pwm_R; // 左右モータPWM出力
-	balance_control(
-		(float)speed,								 /* 前後進命令(+:前進, -:後進) */
-		(float)turn,								 /* 旋回命令(+:右旋回, -:左旋回) */
-		(float)gyro_sensor, /* ジャイロセンサ値 */
-		(float)GYRO_OFFSET,							 /* ジャイロセンサオフセット値 */
-		(float)nxt_motor_get_count(NXT_PORT_C),		 /* 左モータ回転角度[deg] */
-		(float)nxt_motor_get_count(NXT_PORT_B),		 /* 右モータ回転角度[deg] */
-		(float)ecrobot_get_battery_voltage(),		 /* バッテリ電圧[mV] */
-		&pwm_L,										 /* 左モータPWM出力値 */
-		&pwm_R									 /* 右モータPWM出力値 */
-	);
-	nxt_motor_set_speed(NXT_PORT_C, pwm_L, 1); /* 左モータPWM出力セット(-100～100) */
-	nxt_motor_set_speed(NXT_PORT_B, pwm_R, 1); /* 右モータPWM出力セット(-100～100) */
-}
+
 #endif
 //*****************************************************************************
 // 関数名 : line_follow2
